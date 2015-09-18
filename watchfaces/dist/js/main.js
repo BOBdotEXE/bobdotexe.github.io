b4813efe20d55e2831f0faee01a61761bbd62e8b
David <bobdotexe@gmail.com>
2015-09-18 17:55:02 -0400

    bro, i don't even.

---
+++ watchfaces/dist/js/main.js
@@ -0,0 +1,63 @@
+(function() {
+  loadOptions();
+  submitHandler();
+})();
+
+function submitHandler() {
+  var $submitButton = $('#submitButton');
+
+  $submitButton.on('click', function() {
+    console.log('Submit');
+
+    var return_to = getQueryParam('return_to', 'pebblejs://close#');
+    document.location = return_to + encodeURIComponent(JSON.stringify(getAndStoreConfigData()));
+  });
+}
+
+function loadOptions() {
+  var $vib = $('#vib');
+  var $fast = $('#fast');
+  var $med = $('#med');
+  var $slow = $('#fast');
+
+  if (localStorage.vib) {
+    $vib[0].checked = localStorage.vib === 'true';
+	$fast = localStorage.fast
+	$med=localStorage.med;
+    $slow = localStorage.slow;
+  }
+}
+
+function getAndStoreConfigData() {
+  var $vib = $('#vib');
+  var $fast = $('#fast');
+  var $med = $('#med');
+  var $slow = $('#fast');
+
+  var options = {
+    fast: $fast.val(),
+	med: $med.val(),
+	slow: $slow.val(),
+    vib: $vib[0].checked
+  };
+
+  localStorage.vib = options.vib;
+  localStorage.fast = options.fast;
+  localStorage.med = options.med;
+  localStorage.slow = options.slow;
+
+  console.log('Got options: ' + JSON.stringify(options));
+  return options;
+}
+
+function getQueryParam(variable, defaultValue) {
+  var query = location.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+    if (pair[0] === variable) {
+      return decodeURIComponent(pair[1]);
+    }
+  }
+  return defaultValue || false;
+}
